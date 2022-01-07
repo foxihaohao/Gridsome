412c660a78929d5cb35fe81730c02bb75175ac39
佛系豪豪吖 <85242109+foxihaohao@users.noreply.github.com>
2022-01-07 17:59:37 +0800

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,10 +4,12 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>Hello, world!</h1>
+    <h1>欢迎~</h1>
 
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+     Gridsome 是一个由Vue 驱动的静态站点生成器，用于为任何headless CMS、本地文件或API 构建CDN 就绪的网站。
+     <p>
+     使用 Vue.js、webpack 和 Node.js 等现代工具构建网站。从 npm 获取热重载和访问任何包，并在您最喜欢的预处理器（如 Sass 或 Less）中编写 CSS，并带有自动前缀。
     </p>
 
     <p class="home-links">
